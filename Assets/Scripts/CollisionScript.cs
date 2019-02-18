3c687306c64bd542308e6fc6a1e3573a637502b1
kaspervdw <K.vandeWetering@student.han.nl>
2019-02-18 16:36:15 +0100

    .

---
+++ Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CollisionScript : MonoBehaviour
+    {
+
+        public GameObject player;
+        public Material color;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            player = GameObject.Find("Player");
+            color = Resources.Load("Materials/Color") as Material;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnCollisionEnter(Collision collision)
+        { 
+            if (collision.gameObject.name == "Collectible")
+            {
+                Destroy(collision.gameObject);
+                player.GetComponent<MeshRenderer>().material = color;
+            }
+        }
+    }
+}
